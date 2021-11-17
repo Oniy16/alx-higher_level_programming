530e63eab379cbf4ab86621898f14346a80b75d1
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2021-11-16 17:23:18 -0800

    add absolute value

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-x = number % 10
+x = abs(number) % 10
 if number < 0:
-    print("Last digit of {} is {} and is ".format(number, x), end="")
+    x = -x
+print("Last digit of {} is {} and is ".format(number, x), end="")
 if x > 5:
     print("greater than 5")
 elif x == 0:
