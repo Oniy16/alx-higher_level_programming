44b51f319f1fd924b6245a33b13386764871ec0d
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2021-11-16 17:43:19 -0800

    print no 00-89

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(48, 58):
-    x = i+1
-    for x in range(48, 58):
-        print('{}{}'.format(chr(i),chr(x), end=", "))
+for x in range(0, 10):
+    for x2 in range(x + 1, 10):
+        if x == 8 and x2 == 9:
+            print("{}{}".format(x, x2))
+        else:
+            print("{}{}".format(x, x2), end=", ")
