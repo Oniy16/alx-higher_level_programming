ded546a46e18d79769818110d9ee5fdc33fd5992
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2021-11-22 07:10:59 -0800

    add new space b_n row

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for r in range(len(matrix)):
         for c in range(len(matrix[r])):
-            print('{:d}'.format(matrix[r][c]),end = " ")
+            if c < len(matrix[r]) - 1:
+                print('{:d}'.format(matrix[r][c]), end = " ")
+            else:
+                print('{:d}'.format(matrix[r][c]), end = " ")
         print()
