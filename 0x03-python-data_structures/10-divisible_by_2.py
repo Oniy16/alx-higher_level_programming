079c9dbd700ce1f4053ffd85d8c869bea9d97bba
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2021-11-23 05:21:08 -0800

    check valus in list is devisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     result = []
-    for x in range(len(my_list)):
-        if my_list [x] % 2 == 0:
+    for x in my_list:
+        if x % 2 == 0:
             result.append(True)
         else:
             result.append(False)
