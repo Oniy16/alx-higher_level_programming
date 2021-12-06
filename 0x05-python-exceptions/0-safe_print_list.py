9182ec78fe23d6dc1d452e792c2104f5d8872cee
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2021-12-06 14:06:58 -0800

    safe_print_list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
                      if idx < x:
                             print('{}'.format(my_list[idx]), end='')
                             idx += 1
-              print()
+              print('')
        except TypeError:
               pass
        finally:
