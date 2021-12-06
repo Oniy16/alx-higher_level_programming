e587e496173c37448d094147e251cee2015e0352
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2021-12-06 13:21:37 -0800

    safe print divisons

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    c = 0
+    try:
+        c = a / b
+    except zeroDivisionError:
+        result  = None
+    finally:
+        print('Inside result: {}'.format(result))
+        return result
