8d6ed7f7157da4c787c21297f5468def2d4a816c
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2021-12-07 17:52:05 -0800

    add comments and fix error

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-
 class Square:
+    """
+        Square: defines a square
+        Attributes:
+            size (no type or value identification): size of square
+        Method:
+            __init__: init of size attribute for each instance
+    """
 
     def __init__(self, size):
-        """__init__
-        The __init__ method initializes the size value
-        of the square.
-        Attributes:
-            size (int): The size of the square.
         """
+            Initialization of attributes for all instances
+            Args:
+                size (no type): size of the square
+        """
+
         self.__size = size
