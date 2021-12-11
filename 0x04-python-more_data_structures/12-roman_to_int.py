927afd85741d0cf6b2cc43b2276603ae732f67e8
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2021-12-11 08:46:03 -0800

    fun which convert roman no to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+
     roman = {'I':1, 'V':5, 'X':10, 'L':50, 'C': 100, 'D':500, 'M': 1000}
     if not isinstance(roman_string, str):
         return (0)
     no = 0
+
     for x in range(len(roman_string)):
         if roman.get(roman_string[x], 0) == 0:
           return (0)
