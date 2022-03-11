fd06ed602dfd5c1c492d20fec22b3c6dfd553086
Yonatan <yonathanashenafi@gmail.com>
2022-03-11 10:19:33 -0800

    task 5 fix error

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-# Displays all cities of a given state from the
-# states table of the database hbtn_0e_4_usa.
-# Safe from SQL injections.
-# Usage: ./5-filter_cities.py <mysql username> \
-#                             <mysql password> \
-#                             <database name> \
-#                             <state name searched>
-import sys
+'''
+takes in name of state as an arg and lists
+all cities of that state using the db hbtn_0e_4_usa
+'''
+
 import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         password=argv[2],
+                         database=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities INNER JOIN states ON\
+    cities.state_id = states.id WHERE states.name LIKE %(state_name)s\
+    ORDER BY cities.id ASC", {'state_name': argv[4]})
+    res = cur.fetchall()
+    print(', '.join(row[0] for row in res))
+    db.close()
